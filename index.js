eed24930a3d34e4cb145aacd189f7c86d37af3f3
Ben Drucker <bvdrucker@gmail.com>
2016-01-15 08:29:19 -0800

    Implement and test

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var document = require('global/document')
+var Event = require('geval')
+var Keys = require('./keys')
+
+module.exports = Visibility
+
+function Visibility () {
+  var keys = Keys(document)
+  if (!keys) return noopShim()
+
+  return {
+    visible: visible,
+    onChange: Event(listen)
+  }
+
+  function visible () {
+    return !document[keys.hidden]
+  }
+
+  function listen (broadcast) {
+    document.addEventListener(keys.event, function onVisibilityChange () {
+      broadcast(visible())
+    })
+  }
+}
+
+function noopShim () {
+  return {
+    visible: function () {
+      return true
+    },
+    onChange: noop
+  }
+}
+
+function noop () {}
--- test.js
+++ test.js
@@ -1,8 +1,46 @@
 'use strict'
 
 var test = require('tape')
-var Visibility = require('./')
+var proxyquire = require('proxyquire')
+var EventTarget = require('dom-event-target')
 var Keys = require('./keys')
+
+test('supported', function (t) {
+  t.plan(3)
+
+  var document = new EventTarget()
+  document.hidden = false
+
+  var Visibility = proxyquire('./', {
+    'global/document': document
+  })
+
+  var visibility = Visibility()
+
+  t.equal(visibility.visible(), true)
+
+  visibility.onChange(function (visible) {
+    t.equal(visible, false)
+  })
+
+  document.hidden = true
+  document.send('visibilitychange')
+
+  t.equal(visibility.visible(), false)
+})
+
+test('unsupported', function (t) {
+  var Visibility = proxyquire('./', {
+    'global/document': {}
+  })
+
+  var visibility = Visibility()
+
+  t.equal(visibility.visible(), true)
+  t.doesNotThrow(visibility.onChange.bind(null, Boolean))
+
+  t.end()
+})
 
 test('keys', function (t) {
   t.notOk(Keys({}))
